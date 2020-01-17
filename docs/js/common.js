40a24ad11f01075ee4b2b338b9c9649124814ca3
Vova davydiuk <vovkadavidyuk@gmail.com>
2020-01-17 20:49:12 +0200

    add product items and description

--- docs/js/common.js
+++ docs/js/common.js
@@ -1,20 +1,22 @@
-if( $('main')) {
-    $(window).scroll(function() {
-        consoleBG();
-    });
-}  else {
-    $('.header').css('background', '#ffffff');
-    $('.header').css('z-index', '999');
-    $('.header').addClass('active');
-}
-
-function consoleBG() {
-    if ($(window).scrollTop() == 0) {
-        $('.header').css('background', 'none');
-        $('.header').removeClass('active');
-    } else {
+$( document ).ready(function() {
+    if( $('main')) {
+        $(window).scroll(function() {
+            consoleBG();
+        });
+    }  else {
         $('.header').css('background', '#ffffff');
         $('.header').css('z-index', '999');
         $('.header').addClass('active');
     }
-}
+
+    function consoleBG() {
+        if ($(window).scrollTop() == 0) {
+            $('.header').css('background', 'none');
+            $('.header').removeClass('active');
+        } else {
+            $('.header').css('background', '#ffffff');
+            $('.header').css('z-index', '999');
+            $('.header').addClass('active');
+        }
+    }
+});
