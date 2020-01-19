6adcd87f757cfb7e03db8b5810da1fd4685acd1e
Vova davydiuk <vovkadavidyuk@gmail.com>
2020-01-19 14:43:41 +0200

    add all products

--- docs/js/common.js
+++ docs/js/common.js
@@ -31,5 +31,24 @@ $( document ).ready(function() {
     }
 
 
+    'use strict';
+    let btns = Array.from(document.querySelectorAll('.filter-block .filter-btn'));
+    let prgs = Array.from(document.querySelectorAll('.products-wrap'));
+
+    btns.map(function (btn) {
+        btn.addEventListener('click', function () {
+            // $('.filter-block .filter-btn').removeClass('active');
+            let _this = this;
+
+            btns.map(function (btn) {
+                _this.dataset.id == btn.dataset.id ? btn.style.background = '#ffffff' : btn.style.background = 'inherit';
+            });
+
+            prgs.map(function (prg) {
+                _this.dataset.id == prg.dataset.id ? prg.style.display = 'block' : prg.style.display = "none";
+            });
+        });
+    });
+
 });
 
